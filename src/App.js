aa26836f06998c52a29a4c63b3be822fb1b4c9aa
MahimaGajiwala29 <f201506100110093@gmail.com>
2020-10-10 14:14:50 +0530

    Initial Commmit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import ListItem from "./ListItem";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+class App extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default App;
